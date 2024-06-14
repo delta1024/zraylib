226a326f6b98803d09f9656db83f66f3b56caee0
Jake Stannix <jakestannix@gmail.com>
2024-06-14 15:33:18 -0600

    adapted raylib for new modue in main

--- src/main.zig
+++ src/main.zig
@@ -1,67 +1,42 @@
 const std = @import("std");
-const ray = @cImport({
-    @cInclude("raylib.h");
-});
-const draw = @import("draw.zig");
-const Circle = draw.Circle;
-const CircleGrad = draw.CircleGradient;
-const CircleLine = draw.CircleLines;
-const Drawable = draw.Drawable;
-
+const raylib = @import("raylib");
+const Color = raylib.Color;
+const colors: [3]Color = bk: {
+    const c = raylib.colors;
+    break :bk .{ c.Red, c.Blue, c.Green };
+};
+const screen = struct {
+    pub const width = 800;
+    pub const height = 450;
+};
+const fps: u32 = 60;
+const State = struct {
+    frame: u32,
+    frame_count: u32,
+    pub fn tick(self: *State) void {
+        self.frame += 1;
+        if (self.frame > fps) {
+            self.frame = 0;
+            self.frame_count += 1;
+        }
+        if (self.frame_count > 2) {
+            self.frame_count = 0;
+        }
+    }
+};
 pub fn main() !void {
-    const screen_width: i32 = 800;
-    const screen_height: i32 = 450;
+    var state = State{ .frame = 0, .frame_count = 0 };
+    raylib.initWindow(screen.width, screen.height, "Jarasic Tycoon");
+    defer raylib.closeWindow();
 
-    var circle = Circle{
-        .radius = 35,
-        .color = ray.DARKBLUE,
-    };
-    var c_line = CircleLine{
-        .radius = 80,
-        .color = ray.DARKBLUE,
-    };
-    var c_grade = CircleGrad{
-        .radius = 60,
-        .color1 = ray.GREEN,
-        .color2 = ray.SKYBLUE,
-    };
+    raylib.setTargetFps(fps);
 
-    const circles = [_]struct { loc: draw.Location, draw: Drawable }{
-        .{
-            .loc = .{ .x = screen_width / 5, .y = 120 },
-            .draw = circle.drawable(),
-        },
-        .{
-            .loc = .{ .x = screen_width / 5, .y = 220 },
-            .draw = c_grade.drawable(),
-        },
-        .{
-            .loc = .{ .x = screen_width / 5, .y = 340 },
-            .draw = c_line.drawable(),
-        },
-    };
-    var text = draw.Text{
-        .text = "I drew some circles",
-        .font_size = 20,
-        .color = ray.LIGHTGRAY,
-    };
-    ray.InitWindow(screen_width, screen_height, "Jarasic Tycoon");
-    defer ray.CloseWindow();
+    while (!raylib.windowShouldClose()) {
+        state.tick();
+        raylib.beginDrawing();
+        defer raylib.endDrawing();
+        raylib.clearBackground(raylib.colors.White);
 
-    ray.SetTargetFPS(60);
-
-    while (!ray.WindowShouldClose()) {
-        ray.BeginDrawing();
-
-        ray.ClearBackground(ray.RAYWHITE);
-
-        text.draw(.{
-            .x = 350,
-            .y = 150,
-        });
-        for (circles) |pos|
-            pos.draw.draw(pos.loc);
-
-        ray.EndDrawing();
+        raylib.drawRectangle((screen.width / 2) - 32, (screen.height / 2) - 32, 32, 32, colors[state.frame_count]);
     }
 }
