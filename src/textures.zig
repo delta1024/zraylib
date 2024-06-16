8eb064d7bfd26a351f14d94e5ec0bcee9a3aa609
Jake Stannix <jakestannix@gmail.com>
2024-06-16 11:19:33 -0600

    added more texture functions

--- src/textures.zig
+++ src/textures.zig
@@ -1,6 +1,7 @@
 const zty = @import("structs.zig");
 const Image = zty.Image;
 const Texture2D = zty.Texture2D;
+const Color = zty.Color;
 const ray = @cImport(@cInclude("raylib.h"));
 pub fn unloadImage(image: Image) void {
     ray.UnloadImage(@bitCast(image));
@@ -10,4 +11,11 @@ pub fn loadTextureFromImage(image: Image) Texture2D {
 }
 pub fn unloadTexture(texture: Texture2D) void {
     ray.UnloadTexture(@bitCast(texture));
+}
+pub fn drawTexture(texture: Texture2D, x: i32, y: i32, tint: Color) void {
+    ray.DrawTexture(@bitCast(texture), x, y, @bitCast(tint));
+}
+
+pub fn fade(color: Color, alpha: f32) Color {
+    return @bitCast(ray.Fade(@bitCast(color), alpha));
 }
