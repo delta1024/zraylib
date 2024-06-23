c735db33455bad7f59328a5165765c9cb269a654
Jake Stannix <jakestannix@gmail.com>
2024-06-23 17:03:09 -0600

    updated archive script

--- archive.sh
+++ archive.sh
@@ -5,11 +5,11 @@ if [ 1 -ne $# ]; then
 fi
 set -x
 # archive main directory
-git archive --format tar -o release.tar $@
+git archive --format tar -o raylib.tar $@
 # decend recursively and archive each submodule
 git submodule --quiet foreach --recursive \
 	'git archive --format tar --prefix=$displaypath/ -o submodule.tar HEAD'
 # concatenate with main archive
-TOPDIR=$(pwd) git submodule --quiet foreach --recursive 'cd $TOPDIR; tar --concatenate --file=release.tar $displaypath/submodule.tar; rm -fv $displaypath/submodule.tar'
-gzip -9 release.tar
+TOPDIR=$(pwd) git submodule --quiet foreach --recursive 'cd $TOPDIR; tar --concatenate --file=raylib.tar $displaypath/submodule.tar; rm -fv $displaypath/submodule.tar'
+gzip -9 raylib.tar
 
